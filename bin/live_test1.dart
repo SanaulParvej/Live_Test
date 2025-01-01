9500f9222c3ff8f8ceaab284fa54fac56caa2d66
SanaulParvej <perves15-6433@s.diu.edu.bd>
2025-01-01 23:08:07 +0600

    update

--- bin/live_test1.dart
+++ bin/live_test1.dart
@@ -1,4 +1,17 @@
-main(){
-  print("prvj");
-  ertyuiop
+import 'dart:io';
+
+void main() {
+  triangle();
+}
+
+void triangle() {
+  print('Enter  base of the triangle: ');
+  double base = double.parse(stdin.readLineSync()!);
+
+  print('Enter  height of the triangle: ');
+  double height = double.parse(stdin.readLineSync()!);
+
+  double area = 0.5 * base * height;
+
+  print('The area of the triangle is: $area');
 }
